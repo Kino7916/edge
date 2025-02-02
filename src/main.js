70bb008ac450efbe85990f9d4ff94f6f56f6c41a
Mlad <109038115+eolthar@users.noreply.github.com>
2025-02-02 20:49:04 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+const { ForgeExtension } = require('@tryforge/forgescript');
 
+class QuorielEdge extends ForgeExtension {
+    name = "QuorielEdge";
+    description = require("../package.json").description;
+    version = require("../package.json").version;
+
+    init() {
+        this.load(__dirname + '/functions');
+    }
+}
+
+exports.QuorielEdge = QuorielEdge;
