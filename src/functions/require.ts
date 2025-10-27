652a675a5c468ada536f7503f0f9a666759b5021
arfadinov <arfadinov@gmail.com>
2025-10-27 15:52:26 +0700

    Change source to typescript

---
+++ src/functions/require.ts
@@ -0,0 +1,31 @@
+import { NativeFunction, ArgType } from "@tryforge/forgescript";
+import { resolve } from 'path'
+import { existsSync } from "fs";
+
+exports.default = new NativeFunction({
+    name: "$require",
+    description: "Dynamically loads the module",
+    version: "1.0.0",
+    output: ArgType.Unknown,
+    brackets: true,
+    unwrap: true,
+    args: [
+        {
+            name: "path",
+            description: "Module path",
+            type: ArgType.String,
+            required: true,
+            rest: false
+        }
+    ],
+    async execute(ctx, [path]) {
+        const full = resolve(process.cwd(), path);
+        if (!existsSync(full)) return this.stop();
+        try {
+            const result = require(full);
+            return this.success(typeof result === "object" ? JSON.stringify(result) : result);
+        } catch {
+            return this.success();
+        }
+    }
+});
